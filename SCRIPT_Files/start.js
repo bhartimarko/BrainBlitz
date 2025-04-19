91319e37dcef8974441732f48911c39ebc98e27b
bhartimarko <bhartimarko476@gmail.com>
2025-04-19 15:40:07 +0530

    adding new Scripts file for logic

---
+++ SCRIPT_Files/start.js
@@ -0,0 +1,37 @@
+new FinisherHeader({
+    "count": 10,
+    "size": {
+      "min": 659,
+      "max": 1222,
+      "pulse": 1.6
+    },
+    "speed": {
+      "x": {
+        "min": 0.1,
+        "max": 0.6
+      },
+      "y": {
+        "min": 0.1,
+        "max": 0.6
+      }
+    },
+    "colors": {
+      "background": "#f1f622",
+      "particles": [
+        "#fbff48",
+        "#000000",
+        "#2235e5",
+        "#ccc293",
+        "#ca7373"
+      ]
+    },
+    "blending": "overlay",
+    "opacity": {
+      "center": 0.5,
+      "edge": 0.05
+    },
+    "skew": -2,
+    "shapes": [
+      "c"
+    ]
+  });
